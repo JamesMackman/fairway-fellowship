3c6968a51cec95d205db960f9a353cf20c0e90dc
James Mackman <jmackman@live.co.uk>
2024-04-24 15:24:40 +0000

    Create tee time model

--- golftimes/bookings/models.py
+++ golftimes/bookings/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class GolfCourse(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
+
+class TeeTime(models.Model):
+    golf_course = models.ForeignKey(GolfCourse, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    availability = models.IntegerField(default=0)
+   
